f637a06b83fc5827a2a843b667dcdb246eb55f5c
Panteha <panteha.ahmadi@gmail.com>
2017-05-15 16:41:25 +0100

    Number 5: top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,12 @@ class Oystercard
 
 attr_reader :balance
 
-  def initialize(balance = 0)
-     @balance = balance
+  def initialize
+     @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,8 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it "should add 100 to the balance when we top up 100" do
+  subject.top_up(100)
+  expect(subject.balance).to eq 100
+  end
 end
